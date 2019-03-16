fb9508f5f7ca0253ce77f259cf797a3a9a4d048d
Alexander Klassanov <klassanov@gmail.com>
2019-03-16 23:13:31 +0200

    Generate well-formed xml from razor template

--- XMLGenerator/Program.cs
+++ XMLGenerator/Program.cs
@@ -1,8 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using RazorEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
+using RazorEngine.Text;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+using XMLGenerator.Model;
+using XMLGenerator.Extensions;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace XMLGenerator
 {
@@ -10,6 +17,53 @@ namespace XMLGenerator
     {
         static void Main(string[] args)
         {
+            string templateFileName = @"D:\SW Development\Tests\XMLGenerator\XMLGenerator\XMLTemplate\purchaseOrderTemplate.cshtml";
+            string outputFileName = @"D:\SW Development\Tests\XMLGenerator\XMLGenerator\Result\purchaseOrder.xml";
+
+            PurchaseOrder order = new PurchaseOrder
+            {
+                OrderDate = DateTime.Now,
+                Price = 1234.65m,
+                Quantity = 3,
+                ProductName = "Sweet beer"
+            };
+
+            string templateFile = File.ReadAllText(templateFileName);
+
+            //Generate the result
+            string generatedRazorOutput = Engine.Razor.RunCompile(templateFile, "templateKey", typeof(PurchaseOrder), order);
+            StringBuilder sb = FormatXmlString(generatedRazorOutput);
+
+            File.WriteAllText(outputFileName, sb.ToString());
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+
+        private static StringBuilder FormatXmlString(string generatedRazorOutput)
+        {
+            //Remove newline characters
+            string result = generatedRazorOutput.Replace(Environment.NewLine, string.Empty);
+
+            //Create xDocument
+            XDocument xDocument = XDocument.Parse(result);
+
+            //Trim values
+            xDocument.Root.TrimWhiteSpaceFromValues();
+
+            StringBuilder sb = new StringBuilder();
+            XmlWriterSettings xws = new XmlWriterSettings();
+            xws.OmitXmlDeclaration = true;
+            xws.Indent = true;
+
+            //More possibilites for creating the XmlWriter
+            //Serialize the document to a string builder
+            using (XmlWriter xw = XmlWriter.Create(sb, xws))
+            {
+                xDocument.Save(xw);
+            }
+
+            return sb;
         }
     }
 }
